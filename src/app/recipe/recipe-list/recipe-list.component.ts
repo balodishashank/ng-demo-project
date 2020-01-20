38f951426434f0890d6809723f816aa410ed7062
Shashank Balodi <shabalod@publicisgroupe.net>
2020-01-20 09:30:24 +0530

    project-setup

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test recipe", "This is simply a test", "../../assets/download.jfif"),
+    new Recipe("Another Test recipe", "This is simply a test", "../../assets/download.jfif")
+  ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
+}
